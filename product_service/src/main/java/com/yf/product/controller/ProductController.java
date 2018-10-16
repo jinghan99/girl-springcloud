30bcbd17b61a7c1d3d747e807aeb27af7938bded
jinghan <jinghan99@qq.com>
2018-10-16 23:02:53 +0800

    新增 配置中心

--- product_service/src/main/java/com/yf/product/controller/ProductController.java
+++ product_service/src/main/java/com/yf/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.yf.product.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +21,14 @@ import java.util.concurrent.TimeUnit;
  */
 @RestController
 @RequestMapping("api/v1/product")
+@RefreshScope //动态更新配置
 public class ProductController {
 
     @Value("${server.port}")
     private String port;
+
+    @Value("${env.test}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -38,7 +43,8 @@ public class ProductController {
         Product product = productService.getById(id);
         Product product1 = new Product();
         BeanUtils.copyProperties(product,product1);
-        product1.setName(product.getName()+" 端口来源于 - ："+port);
+        product1.setName(product.getName()+" 端口来源于 - ："+port+ "   env:"+env);
+
         return product1;
     }
 }
