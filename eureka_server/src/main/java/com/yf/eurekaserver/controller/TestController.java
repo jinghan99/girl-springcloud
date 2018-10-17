53bd521e74d6b0eabeea12db40880f80aff95c86
jinghan <jinghan99@qq.com>
2018-10-17 23:11:34 +0800

    测试用

--- eureka_server/src/main/java/com/yf/eurekaserver/controller/TestController.java
+++ eureka_server/src/main/java/com/yf/eurekaserver/controller/TestController.java
@@ -2,6 +2,7 @@ package com.yf.eurekaserver.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @RequestMapping(value = "/test",method = RequestMethod.GET)
+    @ResponseBody
     public String test(){
         return "this is eureka server !";
     }
