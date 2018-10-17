14ce149b0280560ce4e96b68b94ce4f4d6a97812
jinghan <jinghan99@qq.com>
2018-10-17 22:47:39 +0800

    新增 配置中心

---
+++ eureka_server/src/main/java/com/yf/eurekaserver/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.yf.eurekaserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Package com.yf.eurekaserver.controller
+ * @Description: TODO
+ * @author: jingh
+ * @date 2018/10/17 22:40
+ */
+@RestController
+@RequestMapping("eureka")
+public class TestController {
+
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public String test(){
+        return "this is eureka server !";
+    }
+}
