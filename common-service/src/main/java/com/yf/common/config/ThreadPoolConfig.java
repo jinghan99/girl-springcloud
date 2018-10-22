cde5bc13e3cb8a850360c94a4053d289ad521684
jinghan94 <jinghan94@126.com>
2018-10-22 18:07:55 +0800

    优化 配置

--- common-service/src/main/java/com/yf/common/config/ThreadPoolConfig.java
+++ common-service/src/main/java/com/yf/common/config/ThreadPoolConfig.java
@@ -31,10 +31,15 @@ public class ThreadPoolConfig {
         executor.setKeepAliveSeconds(60);
         // 设置默认线程名称
         executor.setThreadNamePrefix("Thread-Pool-");
-        // 设置拒绝策略
-        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
         // 等待所有任务结束后再关闭线程池
         executor.setWaitForTasksToCompleteOnShutdown(true);
+
+        // rejection-policy：当pool已经达到max size的时候，如何处理新任务
+        // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        //执行初始化
+        executor.initialize();
         return executor;
     }
 }
